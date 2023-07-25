72ac44ad2da639e807aa4c2ae54bab6b2460c6f0
Maria Deepthi <mdeepthikaranam@turo.com>
2023-07-25 05:22:44 +0000

    skills

--- src/app/components/Choice.tsx
+++ src/app/components/Choice.tsx
@@ -20,82 +20,111 @@ export default function Choice() {
                 alignItems: 'center',
             }}
         >
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
-                <div
-                    className="max-w-sm mb-10 sm:m-2 lg:m-8 md:m-8 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    {/*<a href="#">*/}
-                    {/*    <img className="rounded-t-lg max-h-50" src="https://source.unsplash.com/random?wallpapers"*/}
-                    {/*         alt=""/>*/}
-                    {/*</a>*/}
-                    <a href="#" className="rounded-t-lg max-h-50 justify-center">
-                        <Image className="turo-logo mx-auto" src={MentorIcon} alt="Mentor Logo"/>
-                    </a>
-                    <div className="p-5">
-                        <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">I want
-                                to be a Mentor</h5>
+            <section className="bg-white dark:bg-gray-200">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
+                    <div
+                        className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+                        <a href="#"
+                           className="bg-blue-100 text-className800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2">
+                            New
                         </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Become a Mentor and share your
-                            knowledge and expertise with aspiring individuals. Make a lasting impact by guiding and
-                            inspiring others on their professional journey.</p>
-                        <div className="max-w grid grid-cols-2 gap-5">
-                            <button type="button"
-                                    onClick={() => signIn('google')}
-                                    className="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-blue-800">
-                                Sign Up
-                                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                     fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"
-                                          d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                </svg>
-                            </button>
+                        <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-thin mb-2">
+                            Turo Mentorship Program
+                        </h1>
+                        <p className="text-lg font-thin text-gray-500 dark:text-white mb-6">Are you ready to take your career to new heights? Our internal mentorship program is designed to empower and elevate our employees' growth. Connect with seasoned professionals, gain personalized insights, and unleash your full potential as you embark on a transformative journey within our supportive community. Whether you are a Mentor sharing your wisdom or a Mentee seeking guidance, together, we will shape a brighter future for your career and our company. Join us now and pave the way to success through mentorship!</p>
+                        <a href="/faqs"
+                           className="inline-flex justiclassNamenter items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-purple-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                            Read more
+                            <svg className="w-3.5 h-3.5 ml-2" className-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                 fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                      stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </a>
+                    </div>
+                    <div className="grid md:grid-cols-2 gap-8">
+                        <div
+                            className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                            <div
+                                className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                {/*<a href="#">*/}
+                                {/*    <img className="rounded-t-lg max-h-50" src="https://source.unsplash.com/random?wallpapers"*/}
+                                {/*         alt=""/>*/}
+                                {/*</a>*/}
+                                <a href="#" className="rounded-t-lg max-h-50 justify-center">
+                                    <Image className="turo-logo mx-auto" src={MentorIcon} alt="Mentor Logo"/>
+                                </a>
+                                <div className="p-5">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">I want
+                                            to be a Mentor</h5>
+                                    </a>
+                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Become a Mentor and share your
+                                        knowledge and expertise with aspiring individuals. Make a lasting impact by guiding and
+                                        inspiring others on their professional journey.</p>
+                                    <div className="max-w grid grid-cols-2 gap-5">
+                                        <button type="button"
+                                                onClick={() => signIn('google')}
+                                                className="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-blue-800">
+                                            Sign Up
+                                            <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                 fill="none" viewBox="0 0 14 10">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                      stroke-width="2"
+                                                      d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                            </svg>
+                                        </button>
 
-                            <Link href="/learnmorementors"
-                               className="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                                Learn More
-                            </Link>
+                                        <Link href="/learnmorementors"
+                                              className="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                            Learn More
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                            <div
+                                className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                {/*<a href="#">*/}
+                                {/*    <img className="rounded-t-lg max-h-50" src="https://source.unsplash.com/random?wallpapers"*/}
+                                {/*         alt=""/>*/}
+                                {/*</a>*/}
+                                <a href="#" className="rounded-t-lg max-h-50 justify-center">
+                                    <Image className="turo-logo mx-auto" src={MenteeIcon} alt="Turo Logo"/>
+                                </a>
+                                <div className="p-5">
+                                    <a href="#">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">I want
+                                            to be a Mentee</h5>
+                                    </a>
+                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Join as a Mentee to access valuable insights and guidance from experienced mentors. Accelerate your growth by learning from those who have been there before.</p>
+                                    <div className="max-w grid grid-cols-2 gap-5">
+                                        <button type="button"
+                                                onClick={() => signIn('google')}
+                                                className="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-blue-800">
+                                            Sign Up
+                                            <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                 fill="none" viewBox="0 0 14 10">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                      stroke-width="2"
+                                                      d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                            </svg>
+                                        </button>
+
+
+                                        <Link href="/learnmorementees"
+                                              className="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                            Learn More
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div
-                    className="max-w-sm sm:m-2 lg:m-8 md:m-8 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    {/*<a href="#">*/}
-                    {/*    <img className="rounded-t-lg max-h-50" src="https://source.unsplash.com/random?wallpapers"*/}
-                    {/*         alt=""/>*/}
-                    {/*</a>*/}
-                    <a href="#" className="rounded-t-lg max-h-50 justify-center">
-                        <Image className="turo-logo mx-auto" src={MenteeIcon} alt="Turo Logo"/>
-                    </a>
-                    <div className="p-5">
-                        <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">I want
-                                to be a Mentee</h5>
-                        </a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Join as a Mentee to access valuable insights and guidance from experienced mentors. Accelerate your growth by learning from those who have been there before.</p>
-                        <div className="max-w grid grid-cols-2 gap-5">
-                            <button type="button"
-                                    onClick={() => signIn('google')}
-                                    className="px-3 py-2 text-sm font-medium text-center inline-flex items-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-blue-800">
-                                Sign Up
-                                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                     fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"
-                                          d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                </svg>
-                            </button>
-
-
-                            <Link href="/learnmorementees"
-                               className="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                                Learn More
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </section>
             <div className="mt-20">
                 <ProTip/>
             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,53 @@ export default function Home() {
   const name = session?.data?.user?.name;
 
   return (
-    <>
-      <div >Welcome {name }</div>
-      <div >Let us know more about you.</div>
-    </>
+    <div>
+      <div>Welcome {name}</div>
+      <div>Let us know more about you.</div>
+        <section className="flex-col grid grid-cols-2">
+            <div className="bg-purple-100 h-screen p-3 text-center justify-center">
+                Mentor
+            </div>
+            <div>
+                <form>
+                    <div className="grid md:grid-cols-2 md:gap-6">
+                        <div className="relative z-0 w-full mb-6 group">
+                            <input type="text" name="floating_first_name" id="floating_first_name"
+                                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                   placeholder=" " required/>
+                            <label htmlFor="floating_first_name"
+                                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First
+                                name</label>
+                        </div>
+                        <div className="relative z-0 w-full mb-6 group">
+                            <input type="text" name="floating_last_name" id="floating_last_name"
+                                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                   placeholder=" " required/>
+                            <label htmlFor="floating_last_name"
+                                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last
+                                name</label>
+                        </div>
+                    </div>
+                    <div className="grid md:grid-cols-2 md:gap-6">
+                        <div className="relative z-0 w-full mb-6 group">
+                            <input type="text" name="floating_skills" id="floating_skills"
+                                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                   placeholder="(Ex. Engineering (Back-end), Engineering (Front-end), Marketing, Sales, Customer Service) " required/>
+
+
+                            <label htmlFor="floating_skills"
+                                   className="block w-full peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Skills
+                                </label>
+                        </div>
+                    </div>
+                    <button type="submit"
+                            className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">Submit
+                    </button>
+                </form>
+            </div>
+
+
+        </section>
+    </div>
   )
 }
